103cc9cba91daa2e47f7b7377bc4563f2d98fb81
rachit069 <rachit694.be22@chitkara.edu.in>
2025-03-06 10:31:30 +0530

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -21,4 +21,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
