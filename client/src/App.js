a89e29b7619ba1367326eeca2888f3de63a7e87b
rachit-walia <rachitwalia3@gmail.com>
2025-04-11 12:37:40 +0530

    chair pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+// import './App.css';
+// import HomeSection from './components/Home';
+// import chairPages from './components/chairPage.jsx';
+// function App() {
+//   return (
+//     <div>
+//       <HomeSection />
+//       <chairPages/>
+//     </div>
+//   );
+// }
+
+// export default App;
 import './App.css';
 import HomeSection from './components/Home';
+import ChairPages from './components/chairPage.jsx';  // Changed to PascalCase and match export name
+
 function App() {
   return (
     <div>
       <HomeSection />
+      <ChairPages />  {/* Changed to PascalCase and added space between components */}
     </div>
   );
 }
