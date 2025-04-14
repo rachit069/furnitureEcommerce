8b08b297db35198a77f1c56907f85e562fa31a36
rachit-walia <rachitwalia3@gmail.com>
2025-04-14 09:37:05 +0530

    NewsLetter page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,3 @@
-// import './App.css';
-// import HomeSection from './components/Home';
-// import ChairPages from './components/chairPage.jsx'; 
-// import WhyChooseUs from './components/chooseUs.jsx';
-// import InteriorDesignPage from './components/InteriorDesign.jsx';
-
-// function App() {
-//   return (
-//     <div>
-//       <HomeSection />
-//       <ChairPages />
-//       <WhyChooseUs />
-//       <InteriorDesignPage />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 // App.js
 
 import './App.css';
